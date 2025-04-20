ee8f549e16371303599d9fe2a363c4ad3f69ba8b
omonefe08 <efekolawole@gmail.com>
2025-04-20 13:17:43 -0500

    pushing a python-test app

--- devops/demo/tests.py
+++ devops/demo/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+# devops/conftest.py
+import pytest
+import os
+import django
+
+# Configure Django settings before tests run
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devops.settings')
+django.setup()
